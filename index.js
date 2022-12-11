c789fc625c0cb4807ff2127c9ec14cc79647cf42
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-11 16:20:27 -0300

    corrigindo get users

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
+const Routes = require('./Routes')
 const app = express()
 app.use(cors())
 app.use(express.json())
-const Routes = require('./Routes')
+
 app.use(Routes)
 
 app.listen(4000,()=>console.log('servidor rodando ...'))
