2f2bfc9390bf8c7e0952096efb05735305451905
unknown <dasilvadeoliveirafabio@gmail.com>
2022-12-13 14:32:02 -0300

    teste de deploy com elephance 3

--- config/conection.js
+++ config/conection.js
@@ -3,7 +3,7 @@ const {DataTypes,Sequelize} = require('sequelize')
 const sequelize = new Sequelize(process.env.USER_DB,process.env.USER_DB,process.env.PASSWORD_DB,{
     host:process.env.HOST_DB,
     dialect:'postgres',
-    // dialectModule: require('mysql2')
+    dialectModule: require('pg')
 });
 
 module.exports=sequelize
