c1f7766664e60ab1e179ebab4432db6ac707e901
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2022-12-18 01:04:03 -0200

    atualizando tabela de likes

--- Controllers/Likes_Controller.js
+++ Controllers/Likes_Controller.js
@@ -9,14 +9,17 @@ exports.getLikes =async (req,res)=>{
 exports.setLikes =async (req,res)=>{
     const { id_Usuarios, id_Postagems } = req.body
 
-    const likeDado = await Likes.findOne({
+    const likeDado = await Likes.findAll({
         where:{
+            id_Postagems,
             id_Usuarios
         }
     })
-    if (likeDado) {
+//    res.json(likeDado)
+    if (likeDado.length > 0) {
         const l = await Likes.destroy({
             where:{
+                id_Postagems,
                 id_Usuarios
             }
         })
