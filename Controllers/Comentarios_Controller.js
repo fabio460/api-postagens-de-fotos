198b200499bb48d5f5d2661d9b7fbca01c489707
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2022-12-18 09:42:12 -0200

    criado crud comentarios

--- Controllers/Comentarios_Controller.js
+++ Controllers/Comentarios_Controller.js
@@ -10,6 +10,39 @@ exports.setComentarios =async (req,res)=>{
     res.json(c)
 }
 
+exports.updateComentarios =async (req,res)=>{
+  const {id,id_Postagems,body,id_Usuarios} = req.body
+  const c = await Comentario.update({
+      body
+  },
+  {
+    where:{
+      id,
+      id_Postagems,
+      id_Usuarios
+    }
+  }
+  )
+  res.json(c)
+}
+
+exports.deleteComentarios =async (req,res)=>{
+  const {id_Postagems,id_Usuarios,id} = req.body
+  try {
+    const c = await Comentario.destroy({
+      where:{
+        id,
+        id_Postagems,
+        id_Usuarios
+      }
+    }
+  )
+  res.json(c)
+  } catch (error) {
+    res.json('falha ao deletar mensagem')
+  }
+}
+
 exports.getComentarios =async (req,res)=>{
     const {id} = req.body
     const c = await Comentario.findAll({
